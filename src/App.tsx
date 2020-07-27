67e9d9e90b617f955620f28da73d0683dfef044d
Aaron Schultz <aschultz@microsoft.com>
2020-07-27 15:03:57 -0700

    Add ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  useTheme,
+  IconButton,
+  Icon,
+  Button,
+  Heading,
+} from "@chakra-ui/core";
 
 function App() {
+  const theme = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box backgroundColor={theme.colors.gray[800]} height="100vh">
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        padding="10px"
+        backgroundColor={theme.colors.gray[600]}
+      >
+        <Heading size="md" color={theme.colors.white}>
+          StarForge
+        </Heading>
+        <Flex direction="row" justifyContent="right">
+          <Button leftIcon="add">Add</Button>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  CSSReset,
+  ThemeProvider,
+  theme as defaultTheme,
+} from "@chakra-ui/core";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+function RenderApp() {
+  const theme = {
+    ...defaultTheme,
+    icons: {
+      ...defaultTheme.icons,
+      // TODO: Add icons here
+    },
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RenderApp />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
